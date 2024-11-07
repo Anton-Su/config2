780827460b8546537f5faac7d03b7eb88c43c818
Antua <suleykin2005@mail.ru>
2024-11-07 21:57:13 +0300

    correct main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     if len(sys.argv) != 2:
-        print("Inctruction: python <.../main.py> <name_package>")
+        print("Inctruction: python <.../main.py> <.../name_package>")
         sys.exit(1)
 
 
