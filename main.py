7739ce0861209539e4c6ce9bddbe8d9a6110a9ed
Antua <suleykin2005@mail.ru>
2024-11-08 01:00:52 +0300

    pip install Pillow

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import sys
 import os
+import subprocess
+from PIL import Image
 
 path_to_name_package = os.getcwd() + "\\dependencies.txt"
 dependencies = {}
+
+
+def render_plantuml_file(uml_text, path_to_uml):
+    with open("vremen.puml", "w") as f:
+        f.write(uml_text)
+    try:
+        subprocess.run(["java", "-jar", path_to_uml, "vremen.puml"], check=True)
+    except subprocess.CalledProcessError as e:
+        print("Ошибка при запуске PlantUML:", e)
+
+
+def transform_to_uml_format():
+    itog = ''
+    for package, deps in dependencies.items():
+        for dep in deps:
+            itog += f"{package} --> {dep}\n"
+    itog = "@startuml\n" + itog + "@enduml\n"
+    return itog
 
 
 def detect_dependencies_recur(file_path, name_package):
@@ -17,41 +37,25 @@ def detect_dependencies_recur(file_path, name_package):
                 break
             elif flag:
                 dependencies[name_package].append(stroka.split(": ")[1])
-                #detect_dependencies_recur(file_path, stroka.split(": ")[1])
     if flag:
         for i in dependencies[name_package]:
             if i not in dependencies:
                 detect_dependencies_recur(file_path, i)
-        # for line in f:
-        #     line = line.strip()
-        #     if line == name_package:
-        #
-        #         pass
-        #     if line:
-        #         if not line.startswith("Depends:"):
-        #             current_package = line
-        #             dependencies[current_package] = []
-        #         else:
-        #             dep_package = line.split(": ")[1]
-        #             dependencies[current_package].append(dep_package)
-    print(dependencies)
 
 
-def main(path_uml, package_name):
+def main(package_name, path_uml):
     if not os.path.exists(path_uml) or not os.path.exists(path_to_name_package):
         print('os error')
         return
     detect_dependencies_recur(path_to_name_package, package_name)
-    # if path_uml
-    # print(path_uml)
-    print(1)
-    pass
+    itog = transform_to_uml_format()
+    render_plantuml_file(itog, path_to_uml)
 
 
 if __name__ == "__main__":
-    path_to_uml = "D:\downloads\plantuml-1.2024.7.jar"
+    path_to_uml = r"C:\Users\Antua\PycharmProjects\config2\plantuml-1.2024.7.jar"
     package_name = "libcurl4"
     if len(sys.argv) == 1: # 3
         #path_uml = sys.argv[1]
         #package_name = sys.argv[2]
-        main(path_to_name_package, package_name)
+        main(package_name, path_to_uml)
